fd3b4c07f95abefde37aed20af4066791d867a09
wleskovar <wleskovar@gmail.com>
2022-05-06 08:14:11 -0300

    implementado el sito del template de trip

--- src/app.js
+++ src/app.js
@@ -13,6 +13,15 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
    res.sendFile(__dirname + '/views/index.html')
 });
+app.get('/index', (req, res) => {
+   res.sendFile(__dirname + '/views/index.html')
+});
+app.get('/generic', (req, res) => {
+   res.sendFile(__dirname + '/views/generic.html')
+});
+app.get('/elements', (req, res) => {
+   res.sendFile(__dirname + '/views/elements.html')
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
